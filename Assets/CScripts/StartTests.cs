c2f22220e0eab7f2c2c3b6fa463704af20b59bf9
throw-out <45587825+throw-out@users.noreply.github.com>
2022-03-08 18:46:42 +0800

    update

--- Assets/CScripts/StartTests.cs
+++ Assets/CScripts/StartTests.cs
@@ -81,7 +81,7 @@ public class StartTests : MonoBehaviour
             scriptPath = UnityEditor.AssetDatabase.GetAllAssetPaths().FirstOrDefault(p => p.EndsWith(scriptName) && UnityEditor.AssetDatabase.GetMainAssetTypeAtPath(p) == typeof(UnityEditor.MonoScript));
 #endif
 
-            return scriptPath != null ? $"[(./pic/code_file.png)](./{scriptPath})" : "(./pic/code_file.png)";
+            return scriptPath != null ? $"![文件](./pic/code_file.png)(./{scriptPath})" : "![文件](./pic/code_file.png)";
         };
 
 
